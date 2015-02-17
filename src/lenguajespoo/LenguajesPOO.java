f716f5ced68888e1b29fb9cdbfa8bcdf1a75161a
gatuzo07 <gatuzo07@10.211.55.3>
2015-02-17 01:15:54 -0600

    Mi primer repositorio

---
+++ src/lenguajespoo/LenguajesPOO.java
@@ -0,0 +1,13 @@
+package lenguajespoo;
+
+/**
+ *
+ * @author gatuzo07
+ */
+public class LenguajesPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Mi primer programa de Java2");
+    }
+    
+}
