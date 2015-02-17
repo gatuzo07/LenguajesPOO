59b698ce86dba7ad1c54ebae7055d67cc0430050
gatuzo07 <gatuzo07@10.211.55.3>
2015-02-17 01:25:52 -0600

    Seguda linea modificada

--- src/lenguajespoo/LenguajesPOO.java
+++ src/lenguajespoo/LenguajesPOO.java
@@ -8,6 +8,10 @@ public class LenguajesPOO {
 
     public static void main(String[] args) {
         System.out.println("Mi primer programa de Java2");
+        System.out.println("Mi seunda modificada");
+        //System = clase
+        //out = metodo
+        //println = atributo 
     }
     
 }
