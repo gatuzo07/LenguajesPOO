ff455be809fbfdf5305f8cea98a19177eb77e717
gatuzo07 <rmendozar@live.com.mx>
2015-03-18 23:43:50 -0600

    Tarea 2. Thread Reloj

---
+++ src/Tarea2ThreadsReloj/Reloj.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tarea2ThreadsReloj;
+
+/**
+ *
+ * @author gatuzo07
+ */
+public class Reloj {
+    
+}
