4a12194391bed99b842c83e6c4fcc54401bb97d5
gatuzo07 <rmendozar@live.com.mx>
2015-03-26 19:31:18 -0600

    EjercicioClase. Serializacion @daton

---
+++ src/Serializacion/Cliente.java
@@ -0,0 +1,45 @@
+package Serializacion;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author gatuzo07
+ * Paso1. A las clases a serializar implementarles la interface Serializable
+ */
+public class Cliente implements Serializable {
+    private String nombre;
+    private String sueldo;
+    private Direccion direccion;
+
+    public Cliente(String nombre, String sueldo, Direccion direccion) {
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+        this.direccion = direccion;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(String sueldo) {
+        this.sueldo = sueldo;
+    }
+    
+}
